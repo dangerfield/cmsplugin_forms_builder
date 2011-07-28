77a24da87576e24cfcc4d09d1751ce3e89de4af3
William Dangerfield <wills@dangerfield.co.uk>
2011-07-28 12:22:37 +0100

    added in email sending, and login requirement. I really dont know what Im doing.

--- cms_plugins.py
+++ cms_plugins.py
@@ -2,8 +2,10 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cmsplugin_forms_builder.models import PluginFormModel
 from django import forms
+from django.core.mail import EmailMessage
 from django.utils.translation import ugettext_lazy as _
 from forms_builder.forms.forms import FormForForm
+from django.conf import settings
 
 class PluginForm(CMSPluginBase):
     model = PluginFormModel
@@ -13,6 +15,9 @@ class PluginForm(CMSPluginBase):
     def render(self, context, instance, placeholder):
         request = context['request']
         form = instance.form
+        if form.login_required and not request.user.is_authenticated():
+            context.update({'form': form})
+            return context
         form_for_form = FormForForm(form)
         form_for_form.fields['cms_form_id'] = forms.CharField(initial=form.id, widget=forms.HiddenInput)
         
@@ -20,7 +25,28 @@ class PluginForm(CMSPluginBase):
             if request.method == 'POST'  and int(request.POST.get('cms_form_id',0)) == form.id:
                 form_for_form = FormForForm(form, request.POST, request.FILES)
                 if form_for_form.is_valid():
-                    form_for_form.save()
+                    entry = form_for_form.save()
+                    fields = ["%s: %s" % (v.label, form_for_form.cleaned_data[k]) for (k, v) in form_for_form.fields.items()]
+                    subject = form.email_subject
+                    if not subject:
+                        subject = "%s - %s" % (form.title, entry.entry_time)
+                    body = "\n".join(fields)
+                    if form.email_message:
+                        body = "%s\n\n%s" % (form.email_message, body)
+                    email_from = form.email_from or settings.DEFAULT_FROM_EMAIL
+                    email_to = form_for_form.email_to()
+                    if email_to and form.send_email:
+                        msg = EmailMessage(subject, body, email_from, [email_to])
+                        msg.send()
+                    email_from = email_to or email_from # Send from the email entered.
+                    email_copies = [e.strip() for e in form.email_copies.split(",")
+                        if e.strip()]
+                    if email_copies:
+                        msg = EmailMessage(subject, body, email_from, email_copies)
+                        for f in form_for_form.files.values():
+                            f.seek(0)
+                            msg.attach(f.name, f.read())
+                        msg.send()
                     context.update({'response': form.response})
                     return context
         except ValueError:
